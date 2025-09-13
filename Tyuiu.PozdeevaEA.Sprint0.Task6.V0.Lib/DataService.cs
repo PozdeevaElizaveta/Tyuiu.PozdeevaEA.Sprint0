4aa5def54b1d4cbd52b83cb4ae7e6cd67ec2d537
PozdeevaElizaveta <Tyuiu.PozdeevaEA@outlook.com>
2025-09-14 00:02:22 +0500

    Task6

---
+++ Tyuiu.PozdeevaEA.Sprint0.Task6.V0.Lib/DataService.cs
@@ -0,0 +1,41 @@
+﻿namespace Tyuiu.PozdeevaEA.Sprint0.Task6.V0.Lib
+{
+    public class DataService
+    {
+        public static object AdditionArray(int[] numvers)
+        {
+            var total = 0;
+            for (var i = 0; i < numvers.Length; i++)
+            {
+                total = total + numvers[i];
+            }
+            return total;
+        }
+        public static object SubstractionArray(int[] numvers)
+        {
+            var total = 0;
+            int index = 0;
+
+            while (index < numvers.Length) 
+            {
+                total = total - numvers[index];
+                    index++;
+            }
+            return total;
+        }
+
+        public static object MultiplicationArray(int[] numvers)
+        {
+            var total = 1;
+            int index = 0;
+            do
+            {
+                total = total * numvers[index];
+                index++;
+            }
+            while (index < numvers.Length);
+
+            return total;
+        }
+    }
+}
