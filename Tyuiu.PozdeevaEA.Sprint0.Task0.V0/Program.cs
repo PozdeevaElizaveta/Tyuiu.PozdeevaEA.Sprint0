4b1d5baccb263e82c67fc6620595bb2ad9effde8
PozdeevaElizaveta <Tyuiu.PozdeevaEA@outlook.com>
2025-09-13 19:11:40 +0500

    Task1

--- Tyuiu.PozdeevaEA.Sprint0.Task0.V0/Program.cs
+++ Tyuiu.PozdeevaEA.Sprint0.Task0.V0/Program.cs
@@ -4,4 +4,4 @@
     {
         Console.WriteLine("Hello, World!");
     }
-}
+} 
---
+++ Tyuiu.PozdeevaEA.Sprint0.Task1.V0/Program.cs
@@ -0,0 +1,11 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        string name = "Лиза";
+        int age = 22;
+        Console.WriteLine("Здравствуйте, меня зовут " + name + ", мне " + age + " года!");
+        Console.ReadKey();
+    }
+}
